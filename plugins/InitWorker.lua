739663637b0ea0b5d8cfd992f1a91ac8e35d5619
root <744475502@qq.com>
2016-11-02 18:04:04 +0800

    add redis advice timer

--- plugins/InitWorker.lua
+++ plugins/InitWorker.lua
@@ -3,7 +3,9 @@ local HEALTH_CHECK = require "HealthCheck"
 local register_center = require(CONFIG.REGISTER_CENTER)
 local get_datacenter_config = register_center.get_datacenter_config
 local get_upstream_config = register_center.get_upstream_config
-local get_config, health_check
+local advice_center = require(CONFIG.ADVICE_CENTER)
+
+local get_config, health_check, _get_config, hold_advice
 
 health_check = function(premature)
     if premature then return end
@@ -22,9 +24,7 @@ health_check = function(premature)
     ngx.timer.at(CONFIG.HEALTH_CHECK_POLL_INTERVAL, health_check)
 end
 
-get_config = function(premature)
-    if premature then return end
-
+_get_config = function()
     local status, dc_config, code, msg = pcall(get_datacenter_config)
     if status and dc_config then
         CONFIG.DC_CACHE:set(CONFIG.DC_CACHE_KEY, dc_config)
@@ -35,9 +35,36 @@ get_config = function(premature)
             CONFIG.UPSTREAM_CACHE:set(CONFIG.UPSTREAM_CACHE_KEY, up_config)
         end
     end
+end
+
+get_config = function(premature)
+    if premature then return end
+
+    local lock, code, err = CONFIG.UPSTREAMS_LOCK(false)
+    if lock then
+        pcall(_get_config)
+        CONFIG.UPSTREAMS_LOCK(true)
+    end
+
     ngx.timer.at(CONFIG.POLL_INTERVAL, get_config)
+end
+
+hold_advice = function(premature)
+    if premature then return end
+
+    local advice_center_hold = advice_center.hold()
+    while true do
+        message_type = advice_center_hold(true)
+        ngx.log(ngx.ERR, "message_type: "..message_type)
+        local lock, code, err = CONFIG.UPSTREAMS_LOCK(false)
+        if lock then
+            pcall(_get_config)
+            CONFIG.UPSTREAMS_LOCK(true)
+        end
+    end
 end
 
 ngx.timer.at(0, get_config)
 ngx.timer.at(0, health_check)
+ngx.timer.at(0, hold_advice)
 
