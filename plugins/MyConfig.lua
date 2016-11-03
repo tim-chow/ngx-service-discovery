abe74472f61d9430c78c4c0c993c12ad15b376a9
root <744475502@qq.com>
2016-11-03 11:27:15 +0800

    change channel name

--- plugins/MyConfig.lua
+++ plugins/MyConfig.lua
@@ -85,7 +85,7 @@ local _CONFIG = setmetatable({
             REDIS_ADVICE_HOST="127.0.0.1",
             REDIS_ADVICE_PORT=6379,
             REDIS_ADVICE_PASSWORD=nil,
-            REDIS_ADVICE_CHANNEL="/dubbo/cn.timd.dubbo.demo.DemoService/providers",
+            REDIS_ADVICE_CHANNEL="/dubbo/*",
             REDIS_ADVICE_TIMEOUT=10*60*1000, --unit: ms
             REDIS_ADVICE_RECONNECT_DELAY=0.2,
         },
