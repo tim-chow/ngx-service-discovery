797964052b8d7ae3cbf679fc3373306fcf11aaa8
root <744475502@qq.com>
2016-11-07 14:52:00 +0800

    fix bug and refactor

--- plugins/BaseBalancer.lua
+++ plugins/BaseBalancer.lua
@@ -5,9 +5,8 @@ local _M = {}
 local function filter_upstreams(upstreams, host, uri, kick)
     local result = {}
     for _, upstream in pairs(upstreams) do
-        if (host == 
-            upstream.hostname) and ngx.re.match(uri,
-            upstream.uripattern) then
+        if (host == upstream.hostname) and ngx.re.match(uri,
+                upstream.uripattern) then
             if not kick or CONFIG.IS_UPSTREAM_OK(upstream.address) then
                 table.insert(result, upstream)
             end
@@ -37,6 +36,11 @@ end
 function _M.available_upstreams(...)
     local dc_config = CONFIG.DC_CACHE:get(CONFIG.DC_CACHE_KEY)
     local up_config = CONFIG.UPSTREAM_CACHE:get(CONFIG.UPSTREAM_CACHE_KEY)
+
+    if not dc_config or not up_config then
+        return {}, {}, {}
+    end
+
     local target_upstreams = filter_upstreams(
             up_config[dc_config.target] or {}, ...)
     local backup_upstreams = filter_upstreams(
